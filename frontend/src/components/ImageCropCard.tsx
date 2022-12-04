f8670d6e95cd466031eb49fa92ec37c388408cd4
Florian Cassayre <florian.cassayre@gmail.com>
2022-12-04 01:46:34 +0100

    Replace is partially implemented

--- frontend/src/components/ImageCropCard.tsx
+++ frontend/src/components/ImageCropCard.tsx
@@ -1,22 +1,37 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ReactCrop, { Crop } from 'react-image-crop';
 import { Button } from '@mui/material';
+import { ImageMeta } from '../types/ImageMeta';
 
 interface ImageCardProps {
-    imageUrl: string;
+    imageMeta: ImageMeta;
+    onCropConfirm: (crop: Crop) => void;
 }
 
-export const ImageCropCard: React.FC<ImageCardProps> = ({ imageUrl }) => {
+export const ImageCropCard: React.FC<ImageCardProps> = ({ imageMeta, onCropConfirm }) => {
+    const ref = useRef<HTMLImageElement | null>(null);
     const [crop, setCrop] = useState<Crop>();
     const handleConfirmSelection = () => {
-        console.log(crop);
+        if (crop && ref.current) {
+            const image = ref.current;
+            const c = imageMeta.width / image.width;
+            const modifiedCrop: Crop = {
+                x: crop.x * c,
+                y: crop.y * c,
+                width: crop.width * c,
+                height: crop.height * c,
+                unit: crop.unit,
+            };
+            onCropConfirm(modifiedCrop);
+        }
         setCrop(undefined);
     };
     return (
         <>
             <ReactCrop crop={crop} onChange={c => setCrop(c)}>
-                <img src={imageUrl} alt="Preview" />
+                <img src={imageMeta.url} alt="Preview" ref={ref} />
             </ReactCrop>
+            {JSON.stringify(crop)}
             {!!crop && (
                 <Button variant="outlined" onClick={handleConfirmSelection}>
                     Confirm selection
