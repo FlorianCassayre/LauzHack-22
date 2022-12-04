563b51f27ff7e20bc69e3be3f2f39ecce0d0890d
Florian Cassayre <florian.cassayre@gmail.com>
2022-12-04 01:09:12 +0100

    Agreed on the frontend API

--- frontend/src/api/backendApi.ts
+++ frontend/src/api/backendApi.ts
@@ -1,21 +1,45 @@
-import { PostImageResponse } from './types';
+import { GetFile, GetStatus, PostImage, PostReplace } from './types';
 
 const BACKEND_ENDPOINT = 'http://localhost:8080';
 
-export const postImageFile = (imageBlob: Blob): Promise<PostImageResponse> => {
-    const formData = new FormData()
-    formData.append('file', imageBlob, 'image')
+export const postImageFile: PostImage = (imageBlob) => {
+    const formData = new FormData();
+    formData.append('file', imageBlob, 'image');
     return fetch(
         `${BACKEND_ENDPOINT}/file`,
         { method: 'POST', body: formData },
     ).then(r => r.json());
 };
 
-export const mockedPostImageFile = (imageBlob: Blob): Promise<PostImageResponse> => {
+export const mockedPostImageFile: PostImage = (imageBlob) => {
     console.log(imageBlob);
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
             resolve({ fileId: `${Math.random()}` });
         }, 1000)
     })
+};
+
+export const postReplaceAreaImageFile: PostReplace = (fileId: string) => {
+    throw new Error('Not implemented');
+};
+
+export const mockedPostReplaceAreaImageFile: PostReplace = (fileId: string) => {
+    throw new Error('Not implemented');
+};
+
+export const getImageFile: GetFile = (fileId: string) => {
+    throw new Error('Not implemented');
+};
+
+export const mockedGetImageFile: GetFile = (fileId: string) => {
+    throw new Error('Not implemented');
+};
+
+export const getImageFileStatus: GetStatus = (fileId: string) => {
+    throw new Error('Not implemented');
+};
+
+export const mockedGetImageFileStatus: GetStatus = (fileId: string) => {
+    throw new Error('Not implemented');
 };
